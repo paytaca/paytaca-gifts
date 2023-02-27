d40c9644695ff34795121e306fab7c44b24ae429
khirvy019 <khirvy_19@yahoo.com>
2023-02-27 15:35:48 +0800

    add verification files for android & ios app links

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from handlers import api
 import settings
 
 app = Sanic(__name__)
+
+app_association_blueprint = Blueprint('app-site-association', url_prefix='')
+app_association_blueprint.static('/.well-known/apple-app-site-association', './apple-app-site-association.json', name='app-site-association')
+app_association_blueprint.static('/.well-known/assetlinks.json', './assetlinks.json', name='assetlinks')
+
 app.blueprint(api)
+app.blueprint(app_association_blueprint)
 app.config.CORS_ORIGINS = "*"
 Extend(app)
 
